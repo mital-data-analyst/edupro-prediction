79c82e2af649ab164e84af78b97b00c439532ece
mital-data-analyst <mitalb2311@gmail.com>
2026-05-24 23:20:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_absolute_error import mean_squared_error, r2_score
-
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error, r2_score
 # =========================
 # PAGE SETTINGS
 # =========================
